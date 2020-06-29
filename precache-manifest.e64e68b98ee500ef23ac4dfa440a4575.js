e7be1cc3067d09f3741772738ed7e4fa142e1af2
chinmaykarmokar <chinmaykarmokar@gmail.com>
2020-06-30 00:30:14 +0530

    Updates

---
+++ precache-manifest.e64e68b98ee500ef23ac4dfa440a4575.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91b7747b2054fb399b2e91e636cfcba9",
+    "url": "/fonts/index.html"
+  },
+  {
+    "revision": "46645b927bc311b88fcf",
+    "url": "/fonts/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "d40ac3b725d908821cb1",
+    "url": "/fonts/static/css/main.f1dc3593.chunk.css"
+  },
+  {
+    "revision": "46645b927bc311b88fcf",
+    "url": "/fonts/static/js/2.f511b70e.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/fonts/static/js/2.f511b70e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d40ac3b725d908821cb1",
+    "url": "/fonts/static/js/main.30b3509f.chunk.js"
+  },
+  {
+    "revision": "3c5cc8037875b66bd543",
+    "url": "/fonts/static/js/runtime-main.4ea7fe90.js"
+  }
+]);
